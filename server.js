f6491087c21d519dffc755620e21d14ac2cf830d
caesar <fikrilha099@gmail.com>
2024-12-12 13:01:49 +0700

    [update] add create table, get, and post data route

--- server.js
+++ server.js
@@ -6,15 +6,45 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.sendStatus(200)
+app.get('/', async (req, res) => {
+    try {
+        const data = await pool.query(`
+            SELECT * FROM schools
+        `)
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const { name, location } = req.body
-    res.status(200).send({
-        message: `YOUR KEYS WERE ${name} and ${location}`
-    })
+    try {
+        await pool.query(`
+            INSERT INTO schools (name, location) VALUES ($1, $2)
+        `, [name, location])
+        res.sendStatus(201).send({ message: 'Child created successfully!' })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+app.get('/setup', async (req, res) => {
+    try {
+        await pool.query(`
+            CREATE TABLE schools (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100),
+                location VARCHAR(100)
+            )
+        `)
+        res.sendStatus(200).send({ message: 'Table created successfully!' })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
 app.listen(port, () => console.log(`Server has started on port: ${port}`))
