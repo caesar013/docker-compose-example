0e080172460f3631cfd6c1b5eddb45d8ab711ddb
caesar <fikrilha099@gmail.com>
2024-12-12 12:55:40 +0700

    [update] add post route

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const port = 1337
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.sendStatus(200)
+})
+
+app.post('/', (req, res) => {
+    const { name, location } = req.body
+    res.status(200).send({
+        message: `YOUR KEYS WERE ${name} and ${location}`
+    })
 })
 
 app.listen(port, () => console.log(`Server has started on port: ${port}`))
